4cfea3e7dfd65a2e89a28ff283d30ba830ac5179
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-03-19 01:39:27 +0900

    apply AddPlayer component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { ApolloProvider } from '@apollo/client';
 
 import client from './graphql/client';
 import AddTeam from './components/AddTeam';
+import AddPlayer from './components/AddPlayer';
 import Teams from './components/Teams';
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <AddTeam />
+      <AddPlayer />
       <Teams />
     </ApolloProvider>
   );
