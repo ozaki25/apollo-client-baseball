8d147ae910485948169417b47374d987e10808e7
ozaki25 <fs65ugumori@yahoo.co.jp>
2020-03-19 01:07:55 +0900

    rename

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
-import { GET_TEAMS } from '../graphql/query';
+import { GET_TEAMS } from '../graphql/schema';
 
 function Teams() {
   const { loading, error, data } = useQuery(GET_TEAMS);
